82defc9fee6d7becea8af854ac45ee17cb82c9fb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-03 05:29:57 +0000

    Deploy AsbertMa/tracer-app to github.com/AsbertMa/tracer-app.git:gh-pages

---
+++ precache-manifest.ca3f7da89900688d20cdf90e666f7a8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "b1d3f65af593073618f1",
+    "url": "js/chunk-vendors.ec44844a.js"
+  },
+  {
+    "revision": "0b248c1594b2fa4331fe",
+    "url": "js/app.bbb380f2.js"
+  },
+  {
+    "revision": "f50965762f60b69ed2bd62591c34f927",
+    "url": "index.html"
+  },
+  {
+    "revision": "0b248c1594b2fa4331fe",
+    "url": "css/app.6449e288.css"
+  }
+];
